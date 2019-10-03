f9ffb26d0d5a1adcec60a12c47f7b8db4d8dfbe0
Sze Hao <ngszehao123@hotmail.com>
2019-10-03 21:50:36 +0800

    Completed everything.

---
+++ src/Components/Form.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import TextareaAutosize from 'react-autosize-textarea';
+
+export default class Form extends Component {
+	
+	constructor(props){
+		super(props);
+		this.submitForm = this.submitForm.bind(this);
+		this.state = {
+			status : "",
+			value : ""
+		};
+	}
+	
+	 submitForm(ev) {
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        this.setState({ status: "SUCCESS" });
+      } else {
+        this.setState({ status: "ERROR" });
+      }
+    };
+    xhr.send(data);
+  }
+
+	render() {
+		
+		const { status, value } = this.state;
+
+		return (
+			<form 
+				onSubmit={this.submitForm}
+				action="https://formspree.io/mdrqrkvm"
+				method="POST"
+			>
+				<div className="form-start-container" >
+					<div className="name-container">
+						<div className="field-name">Name</div>
+						{/*To remove auto complete issue*/}
+						<input style={{display: "none"}} type="name" name="new-name"/>
+						<input type="text" name="name" autoComplete="new-username"  required />
+					</div>
+					<div className="email-container">
+						<div className="field-name">Email</div>
+						<input type="email" name="email"  autoComplete="new-password" required />
+					</div>
+				</div>
+				<div className="field-name">Message</div>
+				<TextareaAutosize 
+					rows={10}
+					style={{width: "95%"}}
+					onChange={ e => this.setState({value : e.target.value})}
+				/>
+				<input type="hidden" name="message" value={this.state.value}/>
+				<button className="button submit-button">Submit</button>
+			</form>
+		);
+	}
+}
--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import TextareaAutosize from 'react-autosize-textarea';
+import Form from "./Components/Form.js"
 
 export default class Contact extends Component {
 	render() {
@@ -9,24 +9,7 @@ export default class Contact extends Component {
 				<div className="line-white-short"></div>
 				<div className="text">Have a project in mind? Tell me about it.</div>
 				<div className="form-container" >
-					<form action="" >
-							<div className="form-start-container" >
-								<div className="name-container">
-									<div className="field-name">Name</div>
-									<input type="text" name="name"   required />
-								</div>
-								<div className="email-container">
-									<div className="field-name">Email</div>
-									<input type="email" name="email"  autoComplete="new-password" required />
-								</div>
-							</div>
-							<div className="field-name">Message</div>
-							<TextareaAutosize 
-								rows={10}
-								style={{width: "95%"}}
-							/>
-							<div className="button submit-button">Submit</div>
-					</form>
+					<Form />
 				</div>
 			</div>
 		);
