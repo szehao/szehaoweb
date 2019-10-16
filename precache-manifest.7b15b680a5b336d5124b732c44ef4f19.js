26cb4028870519d5f98dbdb72ad149c672b7a4d7
Sze Hao <ngszehao123@hotmail.com>
2019-10-16 13:19:51 +0800

    Updates

---
+++ precache-manifest.7b15b680a5b336d5124b732c44ef4f19.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53f1ebdcb7210907d924a4fa59436cdc",
+    "url": "/szehaoweb/index.html"
+  },
+  {
+    "revision": "040d1a5316a32242ff8a",
+    "url": "/szehaoweb/static/css/main.fc54e15b.chunk.css"
+  },
+  {
+    "revision": "8e7899658f9fd3427bb9",
+    "url": "/szehaoweb/static/js/2.ab71990e.chunk.js"
+  },
+  {
+    "revision": "040d1a5316a32242ff8a",
+    "url": "/szehaoweb/static/js/main.f2b33b1f.chunk.js"
+  },
+  {
+    "revision": "96f30b7e0a2c5688ee44",
+    "url": "/szehaoweb/static/js/runtime-main.884cd56b.js"
+  },
+  {
+    "revision": "c479217205f75fcccd256f84a825053a",
+    "url": "/szehaoweb/static/media/Poly-Regular.c4792172.otf"
+  },
+  {
+    "revision": "a38a54df0089bca4f5109fe44f944051",
+    "url": "/szehaoweb/static/media/Raleway-ExtraBold.a38a54df.ttf"
+  },
+  {
+    "revision": "b1bdea561f247adc2c904f5b24a07c51",
+    "url": "/szehaoweb/static/media/Raleway-Light.b1bdea56.ttf"
+  },
+  {
+    "revision": "580d0778ad254335be45bf58bb449f43",
+    "url": "/szehaoweb/static/media/Raleway-Regular.580d0778.ttf"
+  },
+  {
+    "revision": "17ba6410cbc694808961a988fd4426de",
+    "url": "/szehaoweb/static/media/Raleway-SemiBold.17ba6410.ttf"
+  },
+  {
+    "revision": "c28c70b0022199fc52a13dc5c15ed285",
+    "url": "/szehaoweb/static/media/eatwhat-preview.c28c70b0.png"
+  },
+  {
+    "revision": "7f74ee7a5cf0406867955ed22a696e56",
+    "url": "/szehaoweb/static/media/logo.7f74ee7a.png"
+  }
+]);
