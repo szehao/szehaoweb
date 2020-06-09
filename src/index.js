5dc03c827fe879c68d74d146759dd6a10d175b51
Sze Hao <ngszehao123@hotmail.com>
2020-06-10 00:47:49 +0800

    ignore commit

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
 ReactDOM.render(
-	<Router basename={process.env.PUBLIC_URL}>
+	<Router history={history} basename={process.env.PUBLIC_URL}>
 		<App />
 	</Router>,
 	document.getElementById('root')
